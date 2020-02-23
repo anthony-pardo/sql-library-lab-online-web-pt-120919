44898fb7e5ed17c433f43b5073e056c804ac1f55
Anthony Pardo <pardo.anthony@gmail.com>
2020-02-23 01:55:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM series 
+  "SELECT title FROM series
+  INNER JOIN books
+  ON books.series_id = series.id 
+  
   "
 end
 
