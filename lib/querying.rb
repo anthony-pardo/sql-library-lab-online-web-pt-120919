29a0d6f00ed83c833cb13e8115d18f728a640465
Anthony Pardo <pardo.anthony@gmail.com>
2020-02-23 02:17:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,11 +39,9 @@ def select_series_title_with_most_human_characters
   GROUP BY series.title, characters.species
   HAVING characters.species = 'human'
   ORDER BY COUNT(characters.species) DESC
-  LIMIT 1;
-  
-  "
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(books)"
 end
